13ac108812c43a94572c6491f7ecfdae10463ee6
Luis Otávio de Almeida Meneses <64923849+Luis-Otavio-Almeida@users.noreply.github.com>
2023-10-03 04:34:24 -0300

    implementação put livro
    não finalizada

--- aplicacao/src/main/java/bancoDeDados/projeto/aplicacao/controller/LivroController.java
+++ aplicacao/src/main/java/bancoDeDados/projeto/aplicacao/controller/LivroController.java
@@ -1,13 +1,17 @@
 package bancoDeDados.projeto.aplicacao.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import bancoDeDados.projeto.aplicacao.domain.aluno.Aluno;
 import bancoDeDados.projeto.aplicacao.domain.livro.Livro;
 import bancoDeDados.projeto.aplicacao.domain.livro.LivroRepository;
 import bancoDeDados.projeto.aplicacao.domain.livro.LivroRequest;
@@ -30,6 +34,20 @@ public class LivroController {
 		Livro novoLivro = new Livro(livro);
 		repository.save(novoLivro);
 		return ResponseEntity.ok(novoLivro);
+	}
+	
+	@PutMapping
+	public ResponseEntity atualizarLivro(@RequestBody LivroRequest request) {
+		Optional<Livro> livroParaAtualizar = repository.findById(request.id_livro());
 		
+		if(livroParaAtualizar.isPresent()) {
+			System.out.println("teste");
+			Livro livroAtualizado = livroParaAtualizar.get();
+			livroAtualizado.setNome(request.nome());
+			livroAtualizado.setDisponivel(request.disponivel());
+			livroAtualizado.setAutor(request.autor());
+			
+		}
+		return ResponseEntity.ok(livroParaAtualizar);
 	}
 }
